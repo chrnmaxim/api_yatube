a7930b37e9c6be0bba79cd2efc2eda199f5a041d
chrnmaxim <chrn.maxim@gmail.com>
2024-05-21 19:27:54 +0300

    Update permissions.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,9 +5,10 @@ class AuthorPermission(permissions.BasePermission):
     """Permission to modify the author's content."""
 
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                and request.user.is_authenticated
-               )
+        return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
